925c0e67e7da9b8b677933d42460f6314d17fe48
HAMDOUCHI <mohamed.hamdouchi@capgemini.com>
2024-10-25 13:26:15 +0200

    import aws ressources

--- imports.tf
+++ imports.tf
@@ -4,45 +4,45 @@
 
 import {
   to = module.main.aws_vpc.this[0]
-  id = "vpc-028bbd0216076027a" #VPC
+  id = "vpc-07985c93f0b115784" #VPC
 }
 
 import {
   to = module.main.aws_subnet.public[0]
-  id = "subnet-097cb717285d96f89" #PublicSubnet1
+  id = "subnet-07a2e62aa6487423d" #PublicSubnet1
 }
 
 import {
   to = module.main.aws_subnet.public[1]
-  id = "subnet-0263c2f7f0f3c022b" #PublicSubnet2
+  id = "subnet-0ed0b0f00810253eb" #PublicSubnet2
 }
 
 import {
   to = module.main.aws_internet_gateway.this[0]
-  id = "igw-0d1be4936ebd335d0" #InternetGateway
+  id = "igw-08fca3741a235933d" #InternetGateway
 }
 
 import {
   to = module.main.aws_route.public_internet_gateway[0]
-  id = "rtb-009092bafa65a8d0c_0.0.0.0/0" #DefaultPublicRoute
+  id = "rtb-0019b33fda549b1e4_0.0.0.0/0" #DefaultPublicRoute
 }
 
 import {
   to = module.main.aws_route_table.public[0]
-  id = "rtb-009092bafa65a8d0c" #PublicRouteTable
+  id = "rtb-0019b33fda549b1e4" #PublicRouteTable
 }
 
 import {
   to = module.main.aws_route_table_association.public[0]
-  id = "subnet-097cb717285d96f89/rtb-009092bafa65a8d0c" #PublicSubnet1/PublicRouteTable
+  id = "subnet-07a2e62aa6487423d/rtb-0019b33fda549b1e4" #PublicSubnet1/PublicRouteTable
 }
 
 import {
   to = module.main.aws_route_table_association.public[1]
-  id = "subnet-0263c2f7f0f3c022b/rtb-009092bafa65a8d0c" #PublicSubnet2/PublicRouteTable
+  id = "subnet-0ed0b0f00810253eb/rtb-0019b33fda549b1e4" #PublicSubnet2/PublicRouteTable
 }
 
 import {
   to = aws_security_group.ingress
-  id = "sg-05f06d5c6921e9d4b" #NoIngressSecurityGroup
+  id = "sg-056a3a87a25f97bac" #NoIngressSecurityGroup
 }
